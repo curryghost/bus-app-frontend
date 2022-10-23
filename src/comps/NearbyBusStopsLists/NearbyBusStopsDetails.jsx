ce267f49ee0bb5fcbbfd7d4723b132c64d240e50
curryghost <anikin8911@gmail.com>
2022-10-24 00:48:03 +0800

    temp fix mobile view

--- src/comps/NearbyBusStopsLists/NearbyBusStopsDetails.jsx
+++ src/comps/NearbyBusStopsLists/NearbyBusStopsDetails.jsx
@@ -38,22 +38,22 @@ export const NearbyBusStopsDetails = ({ busStopCode, index, expanded }) => {
         <List>
             {buses.map((value, index) => (
                 <Box key={index}>
-                    <ListItem button color="primary">
+                    <ListItem button color="primary" disableGutters={true}>
                         <ListItemText primary={`Bus: ${value.ServiceNo}`} sx={{ color: 'black' }} />
-                        {isNaN(getMinutes(value.NextBus.EstimatedArrival)) ? <ListItemText inset={true} /> : <>
-                            <span style={{ height: '2rem', width: '.5rem', backgroundColor: checkLoad(value.NextBus.Load), marginRight: '1rem' }}></span>
-                            <img src={value.NextBus.Type === 'SD' ? sd : dd} style={{ height: '2rem' }} alt='bus' />
-                            <ListItemText inset={true} secondary={`${getMinutes(value.NextBus.EstimatedArrival)} mins`} />
+                        {isNaN(getMinutes(value.NextBus.EstimatedArrival)) ? <ListItemText /> : <>
+                            <span style={{ height: '1.5rem', width: '.3rem', backgroundColor: checkLoad(value.NextBus.Load), marginRight: '.3rem' }}></span>
+                            <img src={value.NextBus.Type === 'SD' ? sd : dd} style={{ height: '1.5rem' }} alt='bus' />
+                            <ListItemText secondary={`${getMinutes(value.NextBus.EstimatedArrival)} mins`} />
                         </>}
-                        {isNaN(getMinutes(value.NextBus2.EstimatedArrival)) ? <ListItemText inset={true} /> : <>
-                            <span style={{ height: '2rem', width: '.5rem', backgroundColor: checkLoad(value.NextBus2.Load), marginRight: '1rem' }}></span>
-                            <img src={value.NextBus2.Type === 'SD' ? sd : dd} style={{ height: '2rem' }} alt='bus' />
-                            <ListItemText inset={true} secondary={`${getMinutes(value.NextBus2.EstimatedArrival)} mins`} />
+                        {isNaN(getMinutes(value.NextBus2.EstimatedArrival)) ? <ListItemText /> : <>
+                            <span style={{ height: '1.5rem', width: '.3rem', backgroundColor: checkLoad(value.NextBus2.Load), marginRight: '.3rem' }}></span>
+                            <img src={value.NextBus2.Type === 'SD' ? sd : dd} style={{ height: '1.5rem' }} alt='bus' />
+                            <ListItemText secondary={`${getMinutes(value.NextBus2.EstimatedArrival)} mins`} />
                         </>}
-                        {isNaN(getMinutes(value.NextBus3.EstimatedArrival)) ? <ListItemText inset={true} /> : <>
-                            <span style={{ height: '2rem', width: '.5rem', backgroundColor: checkLoad(value.NextBus3.Load), marginRight: '1rem' }}></span>
-                            <img src={value.NextBus3.Type === 'SD' ? sd : dd} style={{ height: '2rem' }} alt='bus' />
-                            <ListItemText inset={true} secondary={`${getMinutes(value.NextBus3.EstimatedArrival)} mins`} />
+                        {isNaN(getMinutes(value.NextBus3.EstimatedArrival)) ? <ListItemText /> : <>
+                            <span style={{ height: '1.5rem', width: '.3rem', backgroundColor: checkLoad(value.NextBus3.Load), marginRight: '.3rem' }}></span>
+                            <img src={value.NextBus3.Type === 'SD' ? sd : dd} style={{ height: '1.5rem' }} alt='bus' />
+                            <ListItemText secondary={`${getMinutes(value.NextBus3.EstimatedArrival)} mins`} />
                         </>}
                     </ListItem>
                     <Divider />
