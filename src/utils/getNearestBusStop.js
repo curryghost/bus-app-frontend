7c0d0491955fd2467f0a661a42b462c7af906907
curryghost <anikin8911@gmail.com>
2022-10-27 21:09:12 +0800

    fix getNearestBusStop

--- src/utils/getNearestBusStop.js
+++ src/utils/getNearestBusStop.js
@@ -3,20 +3,11 @@ import getDistance from "./getDistance";
 const getNearestBusStop = (geoLatitude, geoLongitude, busStop, unit) => {
     let nearestBusStop = [];
     busStop.forEach((value) => {
-        if (nearestBusStop.length < 30) {
-            nearestBusStop.push({ ...value, distance: getDistance(geoLatitude, geoLongitude, value.Latitude, value.Longitude, unit) })
-            nearestBusStop.sort((a, b) => a.distance - b.distance)
-        }
-        else {
-            nearestBusStop.forEach((nearStop, index) => {
-                const dist = getDistance(geoLatitude, geoLongitude, value.Latitude, value.Longitude, unit)
-                if (nearStop.distance > dist) {
-                    nearestBusStop.splice(index, 1, { ...value, distance: dist })
-                    nearestBusStop.pop()
-                }
-            })
-        }
+        const dist = getDistance(geoLatitude, geoLongitude, value.Latitude, value.Longitude, unit)
+        if (dist < 2)
+            nearestBusStop.push({ ...value, dist })
     })
+    nearestBusStop.sort((a, b) => a.dist - b.dist)
     return nearestBusStop;
 }
 
