c2258896b487b827f5dadeb0a151c7c1deb2cf97
curryghost <anikin8911@gmail.com>
2022-10-27 17:13:14 +0800

    add drag floating searh

--- src/comps/FloatingSearch/FloatingSearch.jsx
+++ src/comps/FloatingSearch/FloatingSearch.jsx
@@ -8,6 +8,7 @@ import './FloatingSearch.scss';
 export const FloatingSearch = ({ input, handleChange }) => {
     const [expand, setExpand] = useState(false);
     const inputRef = useRef(null);
+    const wrapperRef = useRef(null);
 
     const handleClick = (e) => {
         e.stopPropagation();
@@ -22,6 +23,11 @@ export const FloatingSearch = ({ input, handleChange }) => {
             }
     }, [expand])
 
+    const dragHandler = ({ clientX, clientY }) => {
+        wrapperRef.current.style.top = `${clientY}px`
+        wrapperRef.current.style.left = `${clientX}px`
+        console.log(clientX)
+    }
     useEffect(() => {
         expand && document.addEventListener('click', handleListener);
         expand && inputRef.current.focus();
@@ -29,8 +35,8 @@ export const FloatingSearch = ({ input, handleChange }) => {
     }, [expand, handleListener])
 
     return (
-        <span className={'wrapper ' + (expand || 'fade')} >
-            <span className='icon' onClick={handleClick}>
+        <span className={'wrapper ' + (expand || 'fade')} ref={wrapperRef} style={{ top: '80vh', left: '80vw' }} >
+            <span className='icon' onClick={handleClick} draggable onDragEnd={dragHandler}>
                 <SearchIcon sx={{ fontSize: '3rem', position: 'relative', top: '3px', left: '3px' }} />
             </span>
             <input ref={inputRef} value={input} onChange={handleChange} type="text" className={expand ? 'expand' : ''} />
